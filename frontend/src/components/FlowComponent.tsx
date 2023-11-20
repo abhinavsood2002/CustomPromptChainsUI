c13547c8a785a67a06fc53072e8464327f74a6ae
assoo@student.unimelb.edu.au <abhinavsood2002@gmail.com>
2023-11-21 01:04:06 +1100

    feature: Added basic drop and drag functionality using Reactflow examples

--- frontend/src/components/FlowComponent.tsx
+++ frontend/src/components/FlowComponent.tsx
@@ -1,30 +1,107 @@
-import React, { useEffect, useState } from 'react';
-import ReactFlow, { Background, Node } from 'reactflow';
+// const [message, setMessage] = useState<string>('');
+// console.log(`${process.env.REACT_APP_API_URL}/api/hello`);
+// useEffect(() => {
+//   fetch(`${process.env.REACT_APP_API_URL}/api/hello`) // Assuming your Flask server is running locally
+//     .then((response) => response.json())
+//     .then((data) => setMessage(data.message));
+// }, []);
+import React, { useState, useRef, useCallback } from 'react';
+import ReactFlow, {
+  ReactFlowProvider,
+  addEdge,
+  useNodesState,
+  useEdgesState,
+  Controls,
+  Connection,
+  Edge,
+  Background,
+} from 'reactflow';
 import 'reactflow/dist/style.css';
-const nodes: Node[] = [
-    {
-      id: '1', // required
-      data: { label: 'Node 1' }, 
-      position: { x: 100, y: 100 }, // required
-    },
-  ];
-const FlowComponent: React.FC = () => {
-  const [message, setMessage] = useState<string>('');
-  console.log(`${process.env.REACT_APP_API_URL}/api/hello`);
-  useEffect(() => {
-    fetch(`${process.env.REACT_APP_API_URL}/api/hello`) // Assuming your Flask server is running locally
-      .then((response) => response.json())
-      .then((data) => setMessage(data.message));
+import './../css/main.css';
+import Sidebar from './Sidebar';
+
+const initialNodes = [
+  {
+    id: '1',
+    type: 'input',
+    data: { label: 'input node' },
+    position: { x: 250, y: 5 },
+  },
+];
+
+let id = 0;
+const getId = () => `dndnode_${id++}`;
+
+const FlowComponent = () => {
+  const reactFlowWrapper = useRef(null);
+  const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
+  const [edges, setEdges, onEdgesChange] = useEdgesState([]);
+  const [reactFlowInstance, setReactFlowInstance] = useState(null);
+
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges],
+  );
+
+  const onDragOver = useCallback((event) => {
+    event.preventDefault();
+    event.dataTransfer.dropEffect = 'move';
   }, []);
+
+  const onDrop = useCallback(
+    (event) => {
+      event.preventDefault();
+
+      const type = event.dataTransfer.getData('application/reactflow');
+
+      // check if the dropped element is valid
+      if (typeof type === 'undefined' || !type) {
+        return;
+      }
+
+      // reactFlowInstance.project was renamed to reactFlowInstance.screenToFlowPosition
+      // and you don't need to subtract the reactFlowBounds.left/top anymore
+      // details: https://reactflow.dev/whats-new/2023-11-10
+      const position = reactFlowInstance.screenToFlowPosition({
+        x: event.clientX,
+        y: event.clientY,
+      });
+      const newNode = {
+        id: getId(),
+        type,
+        position,
+        data: { label: `${type} node` },
+      };
+
+      setNodes((nds) => nds.concat(newNode));
+    },
+    [reactFlowInstance, setNodes],
+  );
+
   return (
-    <div>
-        <div style={{height: '100vh', width: '100vw'}}>
-            <ReactFlow nodes={nodes}>
-                <Background />
-            </ReactFlow>
+    <div className="dndflow">
+      <ReactFlowProvider>
+        <Sidebar />
+        <div className="reactflow-wrapper" ref={reactFlowWrapper} style={{height: '100vh', width: '100vw'}}>
+          <ReactFlow
+            nodes={nodes}
+            edges={edges}
+            onNodesChange={onNodesChange}
+            onEdgesChange={onEdgesChange}
+            onConnect={onConnect}
+            onInit={setReactFlowInstance}
+            onDrop={onDrop}
+            onDragOver={onDragOver}
+            fitView
+          >
+            <Background/>
+            <Controls />
+          </ReactFlow>
         </div>
+        
+      </ReactFlowProvider>
     </div>
-    );
+  );
 };
 
 export default FlowComponent;
