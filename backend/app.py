4eb3f671f22301fbebce07e660bba78dc008881f
assoo@student.unimelb.edu.au <abhinavsood2002@gmail.com>
2023-11-21 09:53:34 +1100

    feature: Added ability to run single nodes without the use of input.

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,37 @@
-from flask import Flask, jsonify
+from flask import Flask, request
+import langchain
+from langchain.llms import LlamaCpp
+from langchain.chains import LLMChain
+from langchain.prompts import PromptTemplate
+DEFAULT_PROMPT = "RETURN ERROR"
+model_path = "models/tiny-llama-openhermes-1.1b-step-715k-1.5t.q4_k_m.gguf"
+context_window = 100
+llm = LlamaCpp(model_path=model_path, verbose=False, f16_kv=True, n_ctx = context_window)
+llm_chain = LLMChain(
+    llm=llm,
+    prompt = PromptTemplate.from_template(DEFAULT_PROMPT),
+)
 
 app = Flask(__name__)
 
-@app.route('/api/hello', methods=['GET'])
-def hello():
-    return jsonify({'message': 'Hello from Flask!'})
+@app.route('/api/run/chain_node', methods=['GET'])
+def run_chain_node():
+    prompt = request.args.get('prompt')
+    input_ = request.args.get('input')
+
+    prompt_llm = PromptTemplate.from_template(prompt)
+    print(prompt)
+    print(input_)
+
+    llm_chain.prompt = prompt_llm
+    output = llm_chain.predict()
+    llm_chain.prompt = DEFAULT_PROMPT
+    print(output)
+
+    response = {
+        'output': output,
+    }
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
--- frontend/src/components/nodes/ChainNode.tsx
+++ frontend/src/components/nodes/ChainNode.tsx
@@ -1,9 +1,26 @@
-import { Box, Center, Textarea, StackDivider, VStack } from '@chakra-ui/react'
+import { Box, Center, Textarea, StackDivider, VStack, Button, HStack } from '@chakra-ui/react'
 import React from 'react';
 import { Handle, Position } from 'reactflow';
 
 
 function ChainNode({ data, isConnectable }) {
+  const [prompt, setPrompt] = React.useState('')
+  const [input, setInput] = React.useState('')
+  const [output, setOutput] = React.useState('')
+
+  const handleInputChange = (e) => {
+    let inputValue = e.target.value
+    setPrompt(inputValue)
+  }
+  const run = () => {
+    const promptToPass = encodeURIComponent(prompt);
+    const inputToPass = encodeURIComponent(input);
+    const apiUrl = `${process.env.REACT_APP_API_URL}/api/run/chain_node?prompt=${promptToPass}&input=${inputToPass}`
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((result) => setOutput(result.output))
+  };
+
   return (
     <div className="text-updater-node">
         <Handle type="source" 
@@ -21,13 +38,27 @@ function ChainNode({ data, isConnectable }) {
                 spacing={2}
                 >
                     <Box >
-                    Prompt <Textarea placeholder='Enter a prompt to Transform your input' />
+                      <HStack spacing={10} margin={1} marginLeft={10} >
+                        <Box>
+                          Prompt
+                        </Box>
+                        <Box>
+                          <Button colorScheme="blue" onClick={run} h='20px' w='20px'>
+                            Run
+                          </Button>
+                        </Box>
+                      </HStack>
+                      <Textarea 
+                        value={prompt}
+                        onChange={handleInputChange}
+                        placeholder='Enter a prompt to Transform your input' />
+                     
                     </Box>
-                    <Box h='60px'>
-                    Input 
+                    <Box>
+                    Input: {input}
                     </Box>
-                    <Box h='60px'>
-                    Output
+                    <Box>
+                    Output: {output}
                     </Box>
                 </VStack>
             </Center>
